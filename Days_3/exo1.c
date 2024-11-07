eb5f79fd7f0e47a52a10584fb5bcf2caba0e15c5
crastus <crastus.loko@epitech.eu>
2024-11-07 16:01:03 +0100

    exo 1 days 3

---
+++ Days_3/exo1.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include <unistd.h>
+#include<stdlib.h>
+
+char m_putchar (char a)
+{
+    write(1, &a, 1);
+    /*correct je comprend*/
+}
+
+int m_putstr (char *a)
+{
+    int i = 0;
+    for (i = 0; a[i] != '\0'; i++) {
+        m_putchar(a[i]);
+    }
+    /* ah bon? tu retournes 0 et tu me parles de chaine de caractères?*/
+    return 0;
+}
+
+//Cette fonction permet de de faire la différence entre entre un nombre PAIR et IMPAIR
+void pair_impair(int nbr)
+{
+    int n = nbr % 2;
+
+    if (n == 0) {
+        m_putstr("PAIR\n");
+    } else {
+        m_putstr("IMPAIR\n");
+    }
+}
+
+//Cette fonction fait la meme chose que la premiere a la difference que j'ai utilisé les ternaires
+void pair_impair2(int nbr)
+{
+    int n = nbr % 2;
+
+    n == 0 ? m_putstr("PAIR\n") : m_putstr("IMPAIR\n");
+}
